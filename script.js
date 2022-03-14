1e0d342f7b01a9f615328fa4b4f6f82f1ad1d80c
Ugne Mikulyte <ugn3mikulyte@gmail.com>
2022-03-14 22:26:28 +0000

    update

--- script.js
+++ script.js
@@ -7,29 +7,33 @@ userInput.addEventListener("keydown", function (event) {
   let value = userInput.value;
 
   if (event.keyCode == 13) {
-    //   length result
-    let result1 = value * 3.28084;
-    let result2 = value / 3.28084;
-    let getMetersToFeet = value + " meters = " + result1.toFixed(3) + " feet";
-    let getFeetToMeters = value + " feet = " + result2.toFixed(3) + " meters";
-    lengthResult.innerHTML = getMetersToFeet + " | " + getFeetToMeters;
+    if (value.length === 0) {
+      alert("Please Enter a Number");
+    } else {
+      //   length result
+      let result1 = value * 3.28084;
+      let result2 = value / 3.28084;
+      let getMetersToFeet = value + " meters = " + result1.toFixed(3) + " feet";
+      let getFeetToMeters = value + " feet = " + result2.toFixed(3) + " meters";
+      lengthResult.innerHTML = getMetersToFeet + " | " + getFeetToMeters;
 
-    // volume result
-    let result3 = value * 0.264172;
-    let result4 = value / 0.264172;
-    let getLitersToGallons =
-      value + " liters = " + result3.toFixed(3) + " gallons";
-    let getGallonsToLiters =
-      value + " gallons = " + result4.toFixed(3) + " liters";
-    volumeResult.innerHTML = getLitersToGallons + " | " + getGallonsToLiters;
+      // volume result
+      let result3 = value * 0.264172;
+      let result4 = value / 0.264172;
+      let getLitersToGallons =
+        value + " liters = " + result3.toFixed(3) + " gallons";
+      let getGallonsToLiters =
+        value + " gallons = " + result4.toFixed(3) + " liters";
+      volumeResult.innerHTML = getLitersToGallons + " | " + getGallonsToLiters;
 
-    // mass result
-    let result5 = value * 2.20462;
-    let result6 = value / 2.20462;
-    let getKilosToPounds =
-      value + " kilograms = " + result5.toFixed(3) + " pounds";
-    let getPoundsToKilos =
-      value + " pounds = " + result6.toFixed(3) + " kilograms";
-    massResult.innerHTML = getKilosToPounds + " | " + getPoundsToKilos;
+      // mass result
+      let result5 = value * 2.20462;
+      let result6 = value / 2.20462;
+      let getKilosToPounds =
+        value + " kilograms = " + result5.toFixed(3) + " pounds";
+      let getPoundsToKilos =
+        value + " pounds = " + result6.toFixed(3) + " kilograms";
+      massResult.innerHTML = getKilosToPounds + " | " + getPoundsToKilos;
+    }
   }
 });
